122a0d4a6776901551ea77b5e9d40f1acc617e9b
shelbykohring <71856110+kohringsw@users.noreply.github.com>
2021-03-24 11:06:26 -0500

    add modal features open/close

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Gallery from "./components/Gallery";
 import ContactForm from "./components/Contact";
 
 function App() {
-  const [contactSelected, setContactSelected] = useState(false);
   const [categories] = useState([
     {
       name: "commercial",
@@ -21,6 +20,8 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
